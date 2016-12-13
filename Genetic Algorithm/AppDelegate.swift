c13d6eb081fa3c93cac505a4a032cf717e896df3
Orlando Amorim <orlandoamorim@MacBook-Pro-de-Orlando.local>
2016-12-13 10:38:28 -0300

    Initial Commit

---
+++ Genetic Algorithm/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Genetic Algorithm
+//
+//  Created by Orlando Amorim on 13/12/16.
+//  Copyright © 2016 Orlando Amorim. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Genetic Algorithm/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Genetic Algorithm
+//
+//  Created by Orlando Amorim on 13/12/16.
+//  Copyright © 2016 Orlando Amorim. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
